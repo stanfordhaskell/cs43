204f8ce0436d84129d33494f1fcff43349f82d18
Isaac Scheinfeld <ischeinfeld@Isaacs-MacBook-Pro.local>
2019-01-06 18:47:34 -0500

    Implemented sidenotes using markdown's footnote syntax

--- notes/Notes_features.markdown
+++ notes/Notes_features.markdown
@@ -29,7 +29,9 @@ math).
 ## Heading
 
 Lorem ipsum dolor sit amet, nec munere legendos te. Sea putant appellantur eu,
-in prima adhuc sed, labitur dissentiet vis ea. Accusata splendide mei id, his et
+in prima adhuc sed, [^tagname] labitur dissentiet vis ea. Accusata splendide mei id, his et
 eruditi temporibus, est novum iudico ei. Eu liber nonumes cotidieque has,
 detracto perfecto per ne, ut eius aperiri eripuit his. In aliquid utroque duo,
 ...
+
+[^tagname]: Sidenote
--- site.hs
+++ site.hs
@@ -1,9 +1,14 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 import           Data.Monoid (mappend)
 import qualified Data.Set    as S
+import           Data.List   (intercalate)
+import           Control.Monad.State
 import           Hakyll
 import           Text.Pandoc.Options
+import           Text.Pandoc.JSON
+import           Text.Pandoc.Walk  (walk, walkM)
 
 --------------------------------------------------------------------------------
 config :: Configuration
@@ -57,5 +62,84 @@ customPandocCompiler =
                           writerExtensions = newExtensions,
                           writerHTMLMathMethod = MathJax ""
                         }
-    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+    in pandocCompilerWithTransform defaultHakyllReaderOptions writerOptions usingSideNotes 
 
+
+-------------------------------------------------------------------------------
+getFirstStr :: [Inline] -> Maybe String
+getFirstStr []                 = Nothing
+getFirstStr (Str text:_      ) = Just text
+getFirstStr (_       :inlines) = getFirstStr inlines
+
+newline :: [Inline]
+newline = [LineBreak, LineBreak]
+
+-- This could be implemented more concisely, but I think this is more clear.
+getThenIncr :: State Int Int
+getThenIncr = do
+  i <- get
+  put (i + 1)
+  return i
+
+-- Extract inlines from blocks
+coerceToInline :: [Block] -> [Inline]
+coerceToInline = concatMap deBlock . walk deNote
+ where
+  deBlock :: Block -> [Inline]
+  deBlock (Plain     ls    ) = ls
+  -- Simulate paragraphs with double LineBreak
+  deBlock (Para      ls    ) = ls ++ newline
+  -- See extension: line_blocks
+  deBlock (LineBlock lss   ) = intercalate [LineBreak] lss ++ newline
+  -- Pretend RawBlock is RawInline (might not work!)
+  -- Consider: raw <div> now inside RawInline... what happens?
+  deBlock (RawBlock fmt str) = [RawInline fmt str]
+  -- lists, blockquotes, headers, hrs, and tables are all omitted
+  -- Think they shouldn't be? I'm open to sensible PR's.
+  deBlock _                  = []
+
+  deNote (Note _) = Str ""
+  deNote x        = x
+
+filterInline :: Inline -> State Int Inline
+filterInline (Note blocks) = do
+  -- Generate a unique number for the 'for=' attribute
+  i <- getThenIncr
+
+  -- Note has a [Block], but Span needs [Inline]
+  let content  = coerceToInline blocks
+
+  -- The '{-}' symbol differentiates between margin note and side note
+  let nonu     = getFirstStr content == Just "{-}"
+  let content' = if nonu then tail content else content
+
+  let labelCls = "margin-toggle" ++ (if nonu then "" else " sidenote-number")
+  let labelSym = if nonu then "&#8853;" else ""
+  let labelHTML =
+        "<label for=\"sn-"
+          ++ show i
+          ++ "\" class=\""
+          ++ labelCls
+          ++ "\">"
+          ++ labelSym
+          ++ "</label>"
+  let label = RawInline (Format "html") labelHTML
+
+  let inputHTML =
+        "<input type=\"checkbox\" id=\"sn-"
+          ++ show i
+          ++ "\" "
+          ++ "class=\"margin-toggle\"/>"
+  let input             = RawInline (Format "html") inputHTML
+
+  let (ident, _, attrs) = nullAttr
+  let noteTypeCls       = if nonu then "marginnote" else "sidenote"
+  let note              = Span (ident, [noteTypeCls], attrs) content'
+
+  return $ Span nullAttr [label, input, note]
+
+filterInline inline = return inline
+
+usingSideNotes :: Pandoc -> Pandoc
+usingSideNotes (Pandoc meta blocks) =
+  Pandoc meta (evalState (walkM filterInline blocks) 0)
