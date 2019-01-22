ea9e989a2b96c529f428e0cd7bff60e70cd65bd1
Isaac Scheinfeld <ischeinfeld@DN0a1f0abe.SUNet>
2019-01-21 21:14:32 -0800

    Added hints

--- starter-code/assignment2/src/Main.hs
+++ starter-code/assignment2/src/Main.hs
@@ -8,13 +8,19 @@ main = do
   putStrLn "hello world"
 
 
-newtype Sum = Sum Int
-newtype Product = Product Int
+newtype Sum = Sum Int deriving (Show)
+newtype Product = Product Int deriving (Show)
 
 instance Semigroup Sum where
     _ <> _ = undefined -- define <>
     
     stimes _ _ = undefined -- define O(1) stimes
+    -- note the type is of stimes :: (Integral b) => b -> a -> a
+    --   To use the first argument as a value of type `Int`, you can use the
+    --   fromIntegral function to cast any `Integral` value to any other `Num`
+    --   value, for example if `x` has a type in typeclass `Integral` then
+    --   `fromIntegral x` can be any type (for example, `Int`) in the typeclass
+    --   `Num`.
     
 instance Semigroup Product where
     _ <> _ = undefined -- define <>
@@ -28,3 +34,4 @@ stimes' n x
   | n == 1    = x
   | otherwise = undefined -- implementation here
   -- you can add guards or local functions with "where"
+  -- hint, use the `even` function
