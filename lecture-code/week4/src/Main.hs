b19d409e3e78c9b33b5b79831aa19c34942bb536
Adithya Ganesh <acganesh@stanford.edu>
2019-01-26 15:21:00 -0800

    Add Functor / Main files

--- lecture-code/week4/src/Main.hs
+++ lecture-code/week4/src/Main.hs
@@ -1,5 +1,45 @@
-module Main where
+-- Functor: a typeclass representing
+-- "types that can be mapped over."
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+-- List, Map, Tree are instances of Functor.
+
+-- f is a "container"
+
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+instance Functor [] where
+  fmap :: (a -> b) -> [a] -> [b]
+  fmap g (x:xs) = g x : fmap g xs
+  -- alternatively: fmap = map.
+
+instance Functor Maybe where
+  fmap :: (a -> b) -> Maybe a -> Maybe b
+  fmap _ Nothing = Nothing
+  fmap g (Just a) = Just (g a)
+
+{-instance Functor Either e where-}
+  {-fmap :: (a -> b) -> Either e a -> Either e b-}
+
+instance Functor Tree
+  ...
+
+instance Functor Map
+  ...
+
+instance Functor Sequence
+  ...
+
+-- Laws
+-- Sort of like a group "homomorphism"
+fmap id = id
+fmap (g . h) = (fmap g) . (fmap h)
+
+-- Curry intuition
+-- Does not really take two parameters, but takes a single
+-- parameter and returns a function.
+-- fmap :: (a -> b) -> (f a -> f b)
+-- fmap "lifts" a function from the normal world into the "f" world.
+
+-- Monoid: output is independent of the input. 
+-- For example: multiplying 
